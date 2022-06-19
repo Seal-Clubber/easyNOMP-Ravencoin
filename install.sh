1fdce37882d10196676a4c05bace2938b8b74f4a
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-06-19 03:09:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,53 +1,69 @@
 #!/bin/sh
 
 # This is the EasyNOMP install script.
-echo "EasyNOMP install script."
+echo "EasyNOMP-Ravencoin install script."
 echo "Please do NOT run as root, run as the pool user!"
 
 echo "Installing... Please wait!"
 
 sleep 3
 
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo rm -rf /usr/lib/node_modules
+sudo rm -rf node_modules
+sudo apt remove --purge -y nodejs
+sudo rm /etc/apt/sources.list.d/*
+
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+#sudo apt-get dist-upgrade -y
 
-sudo apt-get install -y sudo git nano wget curl ntp build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev git npm nodejs nodejs-legacy libminiupnpc-dev redis-server software-properties-common fail2ban libdb4.8-dev libdb4.8++-dev
+sudo apt install -y apt-transport-https software-properties-common build-essential autoconf pkg-config make gcc g++ screen wget curl ntp fail2ban
+
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+sudo apt update
+sudo apt install -y libdb-dev libdb++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev
+sudo apt install -y sudo git nodejs nginx certbot python3-certbot-nginx redis-server
+
+sleep 3
 
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
-
+sleep 2
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
-
+sleep 2
 sudo systemctl enable ntp
 sudo systemctl start ntp
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+sudo rm -rf ~/.nvm
+sudo rm -rf ~/.npm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
+sudo chown -R $USER:$GROUP ~/.nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install v18.4.0
+nvm use v18.4.0
 
-nvm install v8.1.4
-nvm use v8.1.4
 npm update -g
-
 npm install -g pm2@latest
 npm install -g npm@latest
 
-git clone https://github.com/EasyX-Community/EasyNOMP.git $(pwd)/EasyNOMP
-
-cd EasyNOMP
+git config --global http.https://gopkg.in.followRedirects true
+git clone https://github.com/Seal-Clubber/easyNOMP-Ravencoin
+chmod -R +x easyNOMP-Ravencoin/
+cd easyNOMP-Ravencoin
 
 npm install
-
 npm update
 npm audit fix
 
-./start-pool.sh
-
 
 echo "Installation completed!"
-echo "Please resume installation at the EasyNOMP Wiki: https://github.com/EasyX-Community/EasyNOMP/wiki"
 
 exit 0
